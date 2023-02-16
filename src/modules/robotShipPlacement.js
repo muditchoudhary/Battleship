aa1922e6e1f5b31be1f31ebe579035aae27a419c
Mudit Choudhary <cmudit60@gmail.com>
2023-02-16 17:21:20 +0530

    Add a logic to place robot ship at randome coordinates

--- src/modules/robotShipPlacement.js
+++ src/modules/robotShipPlacement.js
@@ -5,9 +5,44 @@ const RobotShipPlacement = (board) => {
     const PLAYERSHIPS = [];
     let currentShip = 0;
     const myBoard = board;
-    const FIXEDROWS = [0, 3, 7, 9, 2];
-    const FIXEDCOLS = [0, 3, 3, 4, 1];
+    const FIXEDROWS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const FIXEDCOLS = {
+        airCraft: [0, 1, 2, 3, 4, 5],
+        battleship: [0, 1, 2, 3, 4, 5],
+        cruiser: [0, 1, 2, 3, 4, 5, 6],
+        destroyer: [0, 1, 2, 3, 4, 5, 6, 7],
+        submarine: [0, 1, 2, 3, 4, 5, 6, 7, 8],
+    };
     const TOTALSHIPS = 5;
+
+    const randomPlacementCoordinates = (whichShip) => {
+        let coordinates = String(FIXEDROWS[Math.floor(Math.random() * FIXEDROWS.length)]);
+        const idx = FIXEDROWS.indexOf(Number(coordinates));
+        FIXEDROWS.splice(idx, 1);
+        if (whichShip === 0) {
+            coordinates += String(
+                FIXEDCOLS.airCraft[Math.floor(Math.random() * FIXEDCOLS.airCraft.length)]
+            );
+        } else if (whichShip === 1) {
+            coordinates += String(
+                FIXEDCOLS.battleship[Math.floor(Math.random() * FIXEDCOLS.battleship.length)]
+            );
+        } else if (whichShip === 2) {
+            coordinates += String(
+                FIXEDCOLS.cruiser[Math.floor(Math.random() * FIXEDCOLS.cruiser.length)]
+            );
+        } else if (whichShip === 3) {
+            coordinates += String(
+                FIXEDCOLS.destroyer[Math.floor(Math.random() * FIXEDCOLS.destroyer.length)]
+            );
+        } else if (whichShip === 4) {
+            coordinates += String(
+                FIXEDCOLS.submarine[Math.floor(Math.random() * FIXEDCOLS.submarine.length)]
+            );
+        }
+
+        return coordinates;
+    };
 
     const initalizeShips = () => {
         const AIRCRAFTLEN = 6;
@@ -33,8 +68,9 @@ const RobotShipPlacement = (board) => {
         initalizeShips();
         for (let i = 0; i < TOTALSHIPS; i += 1) {
             if (currentShip < 5) {
-                const row = FIXEDROWS[currentShip];
-                const col = FIXEDCOLS[currentShip];
+                const coordinates = randomPlacementCoordinates(i);
+                const row = Number(coordinates[0]);
+                const col = Number(coordinates[1]);
                 myBoard.placeShip(PLAYERSHIPS[currentShip], row, col);
                 currentShip += 1;
                 console.log(myBoard.board);
